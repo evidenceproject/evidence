7cebb4797fd81d65db2b4919f8bad113d274c3f1
pdncal <pduncan2897@esdmadrid.es>
2022-06-06 00:45:17 +0200

    initial commit

---
+++ app/main.js
@@ -0,0 +1,9 @@
+let bg = document.getElementById('visible'),
+    button = document.querySelector('button');
+
+button.addEventListener('click', function () {
+  bg.classList.toggle('hidden');
+}, false);
+
+
+////////
